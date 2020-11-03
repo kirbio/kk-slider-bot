bad3f8626d6c86b79200feeb2e02cf8796de7f85
MSI\Phakawat <mariosunshinekirby@gmail.com>
2020-11-03 14:58:00 +0700

    send queueing message

--- kksliderbot.py
+++ kksliderbot.py
@@ -91,13 +91,12 @@ async def playEvent(channel, url, currentdj, loop=False):
         print('queueing...')
         # player = await yt.YTDLSource.from_url(url,stream=True)
         # song_queue.append((player,currentdj))
+        await channel.send('Queueing...',delete_after=3)
+
         song_list = yt.extract_info(url)
         '''
         song keys : (['id', 'uploader', 'uploader_id', 'uploader_url', 'channel_id', 'channel_url', 'upload_date', 'license', 'creator', 'title', 'alt_title', 'thumbnails', 'description', 'categories', 'tags', 'subtitles', 'automatic_captions', 'duration', 'age_limit', 'annotations', 'chapters', 'webpage_url', 'view_count', 'like_count', 'dislike_count', 'average_rating', 'formats', 'is_live', 'start_time', 'end_time', 'series', 'season_number', 'episode_number', 'track', 'artist', 'album', 'release_date', 'release_year', 'extractor', 'webpage_url_basename', 'extractor_key', 'n_entries', 'playlist', 'playlist_id', 'playlist_title', 'playlist_uploader', 'playlist_uploader_id', 'playlist_index', 'thumbnail', 'display_id', 'requested_subtitles', 'format_id', 'url', 'player_url', 'ext', 'format_note', 'acodec', 'abr', 'container', 'asr', 'filesize', 'fps', 'height', 'tbr', 'width', 'vcodec', 'downloader_options', 'format', 'protocol', 'http_headers'])
         '''
-        #if queueing a playlist, send a notification
-        if len(song_list) > 1:
-            await channel.send('Queueing a playlist...',delete_after=5)
 
         # queue a song / playlist
         len_before = len(song_queue)
