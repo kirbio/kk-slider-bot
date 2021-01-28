c0cfc2388d01bd15a768f17881248501d5f8600d
MSI\Phakawat <mariosunshinekirby@gmail.com>
2021-01-28 20:22:25 +0700

    proper type declaration

--- checks.py
+++ checks.py
@@ -1,9 +1,12 @@
+from discord.ext.commands import Context
+
 import const
 
-def is_admin(ctx):
+
+def is_admin(ctx: Context):
     return ctx.author.display_name in const.ADMIN_LIST
 
-def is_in_same_vc(ctx):
+def is_in_same_vc(ctx: Context):
     if ctx.voice_client: # if bot has joined VC
         if ctx.author.voice and ctx.author.voice.channel == ctx.voice_client.channel: # check if author has joined VC and it's the same one with the bot
             return True
